52032f56aaf6e92966e5afdd11240f0c3784bde9
Min Xuan <63584286+minxuan1509@users.noreply.github.com>
2020-09-10 15:46:45 +0800

    Update BoardFrame.java

--- BoardFrame.java
+++ BoardFrame.java
@@ -8,32 +8,34 @@ import java.awt.event.*;
 import java.util.*;
 
 public class BoardFrame{
+    int width=800;
+    int height=800;
+    JLabel playerLabel = new JLabel("Player to Move : Red / Blue");
+    JButton backButton = new JButton("Back");
+    JButton saveButton= new JButton("Save");
+    JButton helpButton= new JButton("Help");
+    JPanel btnPanel = new JPanel(new GridLayout(10, 1, 10, 10));
+    JPanel layout = new JPanel(new GridBagLayout());
+    JPanel panel = new JPanel(new BorderLayout());
+    JPanel gamePanel = new JPanel();
+
+    JFrame frame = new JFrame("Game Board Frame");
 
     public BoardFrame(){
-        JFrame frame = new JFrame("Game Board Frame");
-        JPanel gamePanel = new JPanel();
-        JPanel panel = new JPanel(new BorderLayout());
         panel.setBorder(new EmptyBorder(2, 3, 2, 3));
-        JPanel layout = new JPanel(new GridBagLayout());
         layout.setBorder(new EmptyBorder(5, 5, 5, 5));
-        JPanel btnPanel = new JPanel(new GridLayout(10, 1, 10, 10));
-        JLabel label = new JLabel("Player to Move : Red / Blue");
-        JButton backButton = new JButton("Back");
-        JButton saveButton= new JButton("Save");
-        JButton helpButton= new JButton("Help");
         btnPanel.add(backButton);
         btnPanel.add(saveButton);
         btnPanel.add(helpButton);
-        btnPanel.add(label);
+        btnPanel.add(playerLabel);
         layout.add(btnPanel);
         panel.add(layout, BorderLayout.EAST);
         gamePanel.add(panel);
         // 1,1 is chess Board 1,2 is toolBar
         gamePanel.setLayout(new GridLayout(1,2));
-        
 
         frame.add(gamePanel);
-        frame.setSize(800,800);
+        frame.setSize(width, height);
         frame.setVisible(true);
     }
 
