5f45a83ba34fa1bc65c6490de029e1229a0022d1
Esther Tan <estherjq.wls@gmail.com>
2020-09-13 01:09:21 +0800

    Update Arrow.java

--- webale/Arrow.java
+++ webale/Arrow.java
@@ -10,21 +10,7 @@ public class Arrow extends Piece{
         movement = new ArrowBackwardMovement();
     }
 
-    // @Override
-    // public boolean canMove(GameBoard gameboard, Coordinate startpoint, Coordinate endpoint) {
-    //     // we can't move the piece to a Spot that
-    //     // has a piece of the same color
-    //     if (endpoint.getChessPiece().getColour() == this.getColour()) {
-    //         return false;
-    //     }
-
-    //     int x = Math.abs(startpoint.getCoorX() - endpoint.getCoorX());
-    //     int y = Math.abs(startpoint.getCoorY() - endpoint.getCoorY());
-    //     if ((x == 1 && y == 0) || (x == 0 && y == 1)) {
-    //         return true;
-    //     } else
-    //         return false;
-    // }
+    @Override
     public boolean canMove(Coordinate[][] coordinate, Coordinate startPoint, Coordinate endPoint) { 
 		return movement.move(coordinate, startPoint, endPoint);
 	} 
