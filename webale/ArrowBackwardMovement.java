2c3d39978fad3ece08a8de77cccd25aba4c3eaab
Esther Tan <estherjq.wls@gmail.com>
2020-09-13 01:06:15 +0800

    Update ArrowMovement

--- webale/ArrowBackwardMovement.java
+++ webale/ArrowBackwardMovement.java
@@ -8,13 +8,45 @@ public class ArrowBackwardMovement implements Movement
                 int sourceCoordinateY = startPoint.getCoorY();
                 int destCoordinateX = endPoint.getCoorX();
                 int destCoordinateY = endPoint.getCoorY();
-                
-                return true;
+
                 //move 1 step backward at one time
+                //on y-axis & backward, so destination Y is behind source Y by 1
                 if (destCoordinateX == sourceCoordinateX && (destCoordinateY == sourceCoordinateY - 1))
                 {
-                        
-            
+                     //check destination got piece or not, got piece cannot move  
+                        if (coordinate[destCoordinateY][destCoordinateX].getChessPiece() != null )
+			{
+				if (coordinate[sourceCoordinateY][sourceCoordinateX].getChessPiece().getIsRedColor() == coordinate[destCoordinateY][destCoordinateX].getChessPiece().getIsRedColor())
+				{
+					return false;
+				}
+                        }
+                        else
+                        {
+                                return true;
+                        }
+                }
+
+                //move 2 step backward at one time
+                //on y-axis & backward, so destination Y is behind source Y by 2
+                if (destCoordinateX == sourceCoordinateX && (destCoordinateY == sourceCoordinateY - 2))
+                {
+                        //check path between destination and source have other pieces or not
+                         if (coordinate[sourceCoordinateY-1][sourceCoordinateX].getChessPiece() == null)
+                        {
+                                //check destination have piece or not, have piece cannot move  
+                                if (coordinate[destCoordinateY][destCoordinateX].getChessPiece() != null )
+			        {
+				        if (coordinate[sourceCoordinateY][sourceCoordinateX].getChessPiece().getIsRedColor() == coordinate[destCoordinateY][destCoordinateX].getChessPiece().getIsRedColor())
+				        {
+					        return false;
+				        }
+                                }
+                        }
+                        else
+                        {
+                                return true;
+                        }
                 }
         }
 }
--- webale/ArrowForwardMovement.java
+++ webale/ArrowForwardMovement.java
@@ -12,11 +12,44 @@ public class ArrowForwardMovement implements Movement
                 int destCoordinateX = endPoint.getCoorX();
                 int destCoordinateY = endPoint.getCoorY();
             
-                //move 1 step backward at one time
-                if (destCoordinateX == sourceCoordinateX && (destCoordinateY == sourceCoordinateY - 1))
+                //move 1 step forward at one time
+                //on y-axis & forward, so destination Y is in front of source Y by 1
+                if (destCoordinateX == sourceCoordinateX && (destCoordinateY == sourceCoordinateY + 1))
                 {
-                        
+                     //check destination got piece or not, got piece cannot move  
+                        if (coordinate[destCoordinateY][destCoordinateX].getChessPiece() != null)
+			{
+				if (coordinate[sourceCoordinateY][sourceCoordinateX].getChessPiece().getIsRedColor() == coordinate[destCoordinateY][destCoordinateX].getChessPiece().getIsRedColor())
+				{
+					return false;
+				}
+                        }
+                        else
+                        {
+                                return true;
+                        }
+                }
 
+                //move 2 step forward at one time
+                //on y-axis & forward, so destination Y is in front of source Y by 2
+                if (destCoordinateX == sourceCoordinateX && (destCoordinateY == sourceCoordinateY + 2))
+                {
+                        //check path between destination and source have other pieces or not
+                         if (coordinate[sourceCoordinateY+1][sourceCoordinateX].getChessPiece() == null)
+                        {
+                                //check destination have piece or not, have piece cannot move  
+                                if (coordinate[destCoordinateY][destCoordinateX].getChessPiece() != null )
+			        {
+				        if (coordinate[sourceCoordinateY][sourceCoordinateX].getChessPiece().getIsRedColor() == coordinate[destCoordinateY][destCoordinateX].getChessPiece().getIsRedColor())
+				        {
+					        return false;
+				        }
+                                }
+                        }
+                        else
+                        {
+                                return true;
+                        }
                 }
         }
 }
