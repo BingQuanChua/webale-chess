40c163b66f00ff3baced235f2eab047363174133
Esther Tan <estherjq.wls@gmail.com>
2020-09-17 02:22:40 +0800

    Update GameController.java
    after admit defeat will exit program

--- webale/GameController.java
+++ webale/GameController.java
@@ -49,6 +49,7 @@ public class GameController {
         public void actionPerformed(ActionEvent e){
             JLabel defeatLabel = new JLabel(homeFrame.getDefeatImageIcon());
             JOptionPane.showMessageDialog(null, defeatLabel, "You have admitted defeat!", JOptionPane.PLAIN_MESSAGE, null);
+            System.exit(0);
         }
     };
 
